9305404e98820842d63b8f0a74a715b203ea3acf
Zhengqian Kuang <dev.kuang@gmail.com>
2022-08-24 11:14:11 -0600

    change RCT_REMAP_METHOD to RCT_EXPORT_METHOD

--- ios/ReactNativeUtils.m
+++ ios/ReactNativeUtils.m
@@ -6,13 +6,19 @@ RCT_EXPORT_MODULE()
 
 // Example method
 // See // https://reactnative.dev/docs/native-modules-ios
-RCT_REMAP_METHOD(multiply,
-                 multiplyWithA:(double)a withB:(double)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
-{
-  NSNumber *result = @(a * b);
+// RCT_REMAP_METHOD(multiply,
+//                  multiplyWithA:(double)a withB:(double)b
+//                  withResolver:(RCTPromiseResolveBlock)resolve
+//                  withRejecter:(RCTPromiseRejectBlock)reject)
+// {
+//   NSNumber *result = @(a * b);
 
+//   resolve(result);
+// }
+
+RCT_EXPORT_METHOD(multiply:(nonnull NSNumber *)a b:(nonnull NSNumber *)b resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+{
+  NSNumber *result = [NSNumber numberWithDouble:([a doubleValue] * [b doubleValue])];
   resolve(result);
 }
 
